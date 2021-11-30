28391d80c0dcb6b4ba552d515585fd1810487f8a
Eebro <2ibrahimalmalki@gmail.com>
2021-11-30 15:41:10 -0500

    before single-painting

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -53,6 +53,21 @@ function runQuery($pdo, $sql, $parameters=array())     {
     }
 }
 
+
+
+function generateRatingStars($rating) {
+  $output = "";
+  for ($i=0; $i<$rating; $i++) {
+    $output .= '<i class="orange star icon"></i>';
+  }
+  for ($i=$rating; $i<5; $i++) {
+    $output .= '<i class="empty star icon"></i>';
+  }
+  
+  return $output;
+}
+
+
 /**
  * Gets all the paintings with their artists, galleries and shapes
  * @param artist the artist of the painting
