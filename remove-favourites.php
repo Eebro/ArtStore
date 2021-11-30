2b935c59c5d5b5f4ce3970a6398573d5832b0b8d
Eebro <2ibrahimalmalki@gmail.com>
2021-11-30 01:20:58 -0500

    remove-favs

--- remove-favourites.php
+++ remove-favourites.php
@@ -1,35 +1,12 @@
 <?php
-  /**
-   * Bardia Parmoun
-   * 101143006
-   */
-
-  require_once('includes/header.inc.php');
-  require_once('includes/art-config.inc.php');
-  require_once('includes/database.inc.php');
-  require_once('includes/classes.inc.php');
-
   session_start();
   
   if ($_SERVER["REQUEST_METHOD"] == "GET"){
-    if (isset($_GET["id"])){
-      $id = $_GET["id"];
-    } else {
-      $id = 441;
-    }
-
-    if (isset($_SESSION['Favourites'])){
-        $favourite_list = $_SESSION['Favourites'];
-    } else {
-        $favourite_list = Array();
-    }
-
-
-    if (isset($favourite_list[$id])){
-      unset($favourite_list[$id]);
-    }
-
-    $_SESSION['Favourites'] = $favourite_list;
+    if (isset($_SESSION['Favourites'])) $lst = $_SESSION['Favourites'];
+  
+    if (isset($lst[$_GET["id"]])) unset($lst[$_GET["id"]]);
+    
+    $_SESSION['Favourites'] = $lst;
 
     header("Location: view-favourites.php");
   }
