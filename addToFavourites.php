70607c2692bd71610f44909d4903a0fea62dc700
Eebro <2ibrahimalmalki@gmail.com>
2021-11-30 01:06:18 -0500

    new add2favs

--- addToFavourites.php
+++ addToFavourites.php
@@ -1,11 +1,8 @@
 <?php
-
-
   include 'includes/database.inc.php';
- include 'includes/include.inc.php';
+  include 'includes/include.inc.php';
   include 'includes/header.inc.php';
 
- 
   define('DBCONNECTION', 'mysql:host=localhost;dbname=art');
   define('DBUSER', 'testuser');
   define('DBPASS', 'mypassword');
@@ -13,21 +10,14 @@
 
 session_start();
 if ( ! empty($_GET['id']) && isset($_GET['id'])) {
-
   if (! isset($_SESSION['favorites'])) {
-    
       $_SESSION['favorites'] = array();
   }
  
   $lstFav = $_SESSION['Favourites'];
-
-
   $img = getPaintingById($_GET["id"]);
-
   $lstFav[$_GET["id"]] = array($_GET["id"], $img->imageFileName, $img->title);
-
   $_SESSION['Favourites'] = $lstFav;
-
 
 }
 header("Location: view-favourites.php");
